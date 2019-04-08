b92c091229ae733223571b159397015f6f132d2e
Agalar Ashurbekov <a.ashurbekov@cm.expert>
2019-04-08 14:59:39 +0300

    опубликовал в npm

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,12 @@
-module.exports = function (api) {
-  api.cache(true);
-
-  const presets = [
-  	"@babel/preset-env"
-  ];
-
-  return {
-    presets
-  };
-}
+module.exports = {
+  presets: [
+    ['@babel/preset-env',
+      {
+        targets: {
+          node: 'current',
+        },
+      },
+    ],
+  ],
+  plugins: [],
+};
